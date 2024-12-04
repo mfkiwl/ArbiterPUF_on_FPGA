5d347b001c3339330e594695d2976ded95faa08c
tatore02 <sasaruocco9@gmail.com>
2024-12-04 15:22:01 +0100

    add randomness_plot

--- code/evaluation/randomness_plot.py
+++ code/evaluation/randomness_plot.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 22})
 
 if __name__ == "__main__":
-    with open('stability.txt') as f:
-        challenge = f.readline().split(' ')[-1].strip()
+    with open('randomness.txt') as f:
+        challenges = f.readline().split(' = ')[-1].strip()
         responses = f.readline().split(' ')[-1].strip()
-
+    
     responses = [int(c) for c in responses]
     num_responses = len(responses)
 
@@ -13,7 +14,6 @@ if __name__ == "__main__":
     plt.figure(figsize=(10, 5))
     plt.plot(x_values, responses, marker='o', linestyle='-', color='b', label='PUF Response')
 
-    plt.suptitle(f"Challenge: {challenge}", fontsize=14)
     plt.xlabel("Test Number")
     plt.ylabel("Response (0 or 1)")
     plt.yticks([0, 1], ['0', '1'])
@@ -22,4 +22,3 @@ if __name__ == "__main__":
 
     plt.tight_layout()
     plt.show()
-    
